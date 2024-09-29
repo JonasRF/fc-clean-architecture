ff7f5179b20fb9cf1a0ef012cb35be3058ea30df
JonasRF <jonasribeiro41@yahoo.com.br>
2024-09-28 23:11:32 -0300

    Usecase create product

---
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -0,0 +1,39 @@
+import CreateProductUseCase from "./create.product.usecase";
+
+const input = {
+  name: "Notebook",
+  price: 2000.0,
+};
+
+const MockRepository = () => {
+  return {
+    find: jest.fn(),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit test create product use case", () => {
+  it("should create a product", async () => {
+    const productRepository = MockRepository();
+    const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+    const output = await productCreateUseCase.execute(input);
+
+    expect(output).toEqual({
+      id: expect.any(String),
+      name: input.name,
+      price: input.price,
+    });
+  });
+
+  it("should throw an error when name is missing", async () => {
+    const productRepository = MockRepository();
+    const usecase = new CreateProductUseCase(productRepository);
+
+    input.name = "";
+
+    await expect(usecase.execute(input)).rejects.toThrow("Name is required");
+  });
+});
